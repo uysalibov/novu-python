b244e3ab169d3448b9323e97881896691beb965c
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2022-12-14 01:05:34 +0300

    Create __init__.py

---
+++ novu/__init__.py
@@ -0,0 +1,14 @@
+"""
+Novu API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Novu API.
+"""
+
+__title__ = "novu"
+__author__ = "novuhq"
+__version__ = "v0.0.1"
+
+__novu_version__ = "v1"
+
+
+from novu import *
